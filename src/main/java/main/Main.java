6c33c5e35a3f9be8c70289a6ca193184423e2322
AlejaGonzalezV <aleja.gonzalezv19@gmail.com>
2018-09-03 16:14:39 -0500

    Class calculator implemented

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.*;
 
 public class Main extends Application{
 
@@ -22,6 +25,15 @@ public class Main extends Application{
 
 	public static void main (String[] args) {
 		launch(args);
+		
+		Calculator cal = new Calculator();
+//		cal.addRational(2,3,3,2);
+		
+		System.out.println(cal.getNumerator());
+		System.out.println(cal.getDenominator());
+		
+		
+		
 		
 		
 	}
--- src/main/java/model/calculator.java
+++ src/main/java/model/calculator.java
@@ -1,5 +1,103 @@
 package model;
 
-public class calculator {
+import numericSystem.RationalNumber;
 
+public class Calculator{
+	
+	private int numerator;
+	private int denominator;
+
+	public Calculator() {
+		
+		
+	}
+	
+	public void addRational(int num1, int num2, int num3, int num4) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		RationalNumber<Number> rational2 = new RationalNumber(num3, num4);
+		rational1.addRational(rational2);
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void substractRational(int num1, int num2, int num3, int num4) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		RationalNumber<Number> rational2 = new RationalNumber(num3, num4);
+		rational1.substractRational(rational2);
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void miltiplyRational(int num1, int num2, int num3, int num4) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		RationalNumber<Number> rational2 = new RationalNumber(num3, num4);
+		rational1.multiplyRational(rational2);
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void divideRational(int num1, int num2, int num3, int num4) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		RationalNumber<Number> rational2 = new RationalNumber(num3, num4);
+		rational1.divideRational(rational2);
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void simplifyRational(int num1, int num2) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		rational1.simplifyRational();
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void squaredRational(int num1, int num2) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		rational1.simplifyRational();
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	public void rationalInverse(int num1, int num2) {
+		
+		RationalNumber<Integer> rational1 = new RationalNumber(num1, num2);
+		rational1.simplifyRational();
+		this.numerator = (int) rational1.getNumerator();
+		this.denominator = (int) rational1.getDenominator();
+		
+	}
+	
+	
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+
+	public int getDenominator() {
+		return denominator;
+	}
+
+	public void setDenominator(int denominator) {
+		this.denominator = denominator;
+	}
+
+	
+	
+	
 }
